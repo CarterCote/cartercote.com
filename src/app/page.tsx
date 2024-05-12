e4e74b10f106bc20d747f16c5e8d68cf13c65920
cartercote <cartercote06@gmail.com>
2024-05-11 21:55:05 -0400

    fixed project import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import React from "react"
 
 import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
-import Projects from "../components/Projects"
 import Footer from "../components/Footer"
 
 const Home = () => {
@@ -10,8 +9,6 @@ const Home = () => {
         <div>
             <Navbar />
             <Hero />
-            {/* <Projects /> */}
-            {/* <Footer /> */}
         </div>
     )
 };
