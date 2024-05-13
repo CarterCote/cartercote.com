32eb7f7e576908c9e0ef47cee6cdd3d9377c2e19
cartercote <cartercote06@gmail.com>
2024-05-13 09:17:03 -0400

    second row of play

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -47,6 +47,22 @@ const Play = () => {
                   </div>
                 </div>
               </div>
+              <div className="flex flex-col pt-10 justify-between md:space-x-10 md:flex-row">
+                <div className="flex flex-col pb-20 md:pb-0 mt-2 md:mt-10 md:pl-20">
+                  <NextImage priority src="/design/003.jpg" height={438} width={350} alt="mixed signals" className="w-full sm:w-auto sm:h-auto"/>
+                  <div className="flex flex-row justify-between mt-3 w-full">
+                    <p className="text-start font-aeonik-bold text-sm tracking-tight">ATLANTA TYPOGRAPHY</p>
+                    <p className="text-start font-aeonik-thin text-sm tracking-tight ml-2">2020</p>
+                  </div>
+                </div>
+                <div className="flex flex-col pb-20 md:pb-0 md:mt-40 md:mb-[420px]">
+                  <NextImage priority src="/design/sxShirtMockup.png" width={600} height={400} alt="sxShirt" className="w-full sm:w-auto sm:h-auto"/>
+                  <div className="flex flex-row justify-between mt-3 w-full">
+                    <p className="text-start font-aeonik-bold text-sm tracking-tight">SX GEORGIA TECH GRAPHIC T-SHIRT</p>
+                    <p className="text-start font-aeonik-thin text-sm tracking-tight ml-2">2022</p>
+                  </div>
+                </div>
+              </div>
               <div className="w-full flex flex-row py-20">
                   <Marquee gradient gradientColor="#000" gradientWidth={25} className="h-[54px] w-full" speed={85} autoFill loop={0}>
                       <p className="w-full font-voyager-thin tracking-tight leading-[100%] text-[54px] mr-8">More</p>
