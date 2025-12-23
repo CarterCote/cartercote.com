4e7c0e9d676138f71daafe902ec588f02c75df14
Carter Cote <59104482+CarterCote@users.noreply.github.com>
2025-12-23 12:22:33 -0500

    add bg to stuff

--- src/app/stuff/page.tsx
+++ src/app/stuff/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
 import StuffContent from "../../components/StuffContent";
 import StuffHeader from "../../components/StuffHeader";
+import VideoBackground from "../../components/sections/VideoBackground";
 import { getAllPosts } from "../../lib/blog";
 
 const Stuff = () => {
@@ -9,6 +10,7 @@ const Stuff = () => {
 
   return (
     <>
+      <VideoBackground />
       <Navbar />
       <div className="flex w-full min-h-screen flex-col pt-32 pb-20 px-8 md:px-20">
         <div className="max-w-[800px] mx-auto w-full">
